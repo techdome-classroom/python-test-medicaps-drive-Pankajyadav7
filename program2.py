1aadee3f3f2ece1c0e5cc07026e23f1164d7cd84
Pankajyadav7 <168063309+Pankajyadav7@users.noreply.github.com>
2024-04-25 14:11:12 +0530

    Initial commit by pankaj yadav
    python code added by pankaj yadav

--- program2.py
+++ program2.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = roman_values[s[-1]]  # Initialize total with the value of the last character
+        
+        for i in range(len(s) - 2, -1, -1):  # Iterate from second to the last character to the first character
+            if roman_values[s[i]] < roman_values[s[i + 1]]:
+                total -= roman_values[s[i]]
+            else:
+                total += roman_values[s[i]]
+        
+        return total
